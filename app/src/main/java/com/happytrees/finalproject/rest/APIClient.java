3393775df639fdd2a9c707ef4e01f25990cf0367
limudimwork <boriskunda@gmail.com>
2018-02-07 14:31:34 +0200

    added radio buttons

---
+++ app/src/main/java/com/happytrees/finalproject/rest/APIClient.java
@@ -0,0 +1,22 @@
+package com.happytrees.finalproject.rest;
+
+
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class APIClient {//class dedicated to Retrofit instance
+    public static final String BASE_URL = "https://maps.googleapis.com";
+    private static Retrofit retrofit = null;
+
+
+    public static Retrofit getClient() {
+        if (retrofit==null) {//if instance of Retrofit is null build new one using GSON  ".addConverterFactory(GsonConverterFactory.create())"  and base url ".baseUrl(BASE_URL)"
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
---
+++ app/src/main/java/com/happytrees/finalproject/rest/Endpoint.java
@@ -0,0 +1,9 @@
+package com.happytrees.finalproject.rest;
+
+/**
+ * Created by Boris on 2/6/2018.
+ */
+
+public class Endpoint {
+    //you can  write @GET of both sites
+}
