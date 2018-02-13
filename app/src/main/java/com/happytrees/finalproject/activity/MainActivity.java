98d44622a2b97a34b4ea724eecdbbf6dab3732f5
limudimwork <boriskunda@gmail.com>
2018-02-13 18:20:14 +0200

    gps

--- app/src/main/java/com/happytrees/finalproject/activity/MainActivity.java
+++ app/src/main/java/com/happytrees/finalproject/activity/MainActivity.java
@@ -1,23 +1,64 @@
 package com.happytrees.finalproject.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.happytrees.finalproject.R;
 import com.happytrees.finalproject.fragments.FragmentA;
 import com.happytrees.finalproject.fragments.FragmentB;
+
+//https://www.google.co.il/maps/@32.0662593,34.7698209,15z --> put here your latitude , longitude
 
 public class MainActivity extends AppCompatActivity {
 
 //SAVE INSTANCE ON ROTATION CHANGE --> YET TO COME
 
+    FusedLocationProviderClient mFusedLocationClient;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set location provider
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    Log.i("MAPS", "Location: " + location.getLatitude() + " " + location.getLongitude());
+                }
+            }
+        });
+
+
+
+
 
         //INIT FRAGMENT A
         FragmentA fragmentA = new FragmentA();//generate a new Fragment
