771b8c87d87a0df5a4812eb6be306d25e63ba4c8
limudimwork <boriskunda@gmail.com>
2018-03-11 17:24:22 +0200

    minor changes

--- app/src/main/java/com/happytrees/finalproject/adapter/SearchHistoryAdapter.java
+++ app/src/main/java/com/happytrees/finalproject/adapter/SearchHistoryAdapter.java
@@ -1,6 +1,9 @@
 package com.happytrees.finalproject.adapter;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.happytrees.finalproject.R;
+import com.happytrees.finalproject.activity.MainActivity;
 import com.happytrees.finalproject.database.LastSearch;
 
 import java.util.List;
@@ -22,6 +26,9 @@ public class SearchHistoryAdapter extends RecyclerView.Adapter <SearchHistoryAda
 //VARIABLES
     public List<LastSearch> lastSearchResults;//list of places results
     public Context context;
+    public String hPreference = "kilometre";//default value of distance measurement units
+    public double roundedHDis;//rounded value of distance (less numbers after dot)
+    public float[] HDistanceResults = new float[10];//10 random number.you need any number higher than 3
 
     public SearchHistoryAdapter(List<LastSearch> lastSearchResults, Context context) {
         this.lastSearchResults = lastSearchResults;
@@ -71,6 +78,37 @@ public class SearchHistoryAdapter extends RecyclerView.Adapter <SearchHistoryAda
 
             TextView historyName = (TextView) myHView.findViewById(R.id.historyName);//NAME
             historyName.setText(currentLastSearch.name);
+
+            TextView historyAddress = (TextView) myHView.findViewById(R.id.historyAddress);//ADDRESS
+            historyAddress.setText(currentLastSearch.formatted_address);
+
+
+            //get lat and lng of current Last Search
+            double keptLat = currentLastSearch.lat;
+            double keptLng = currentLastSearch.lng;
+
+
+            TextView hDistance = (TextView) myHView.findViewById(R.id.historyDistance);//DISTANCE
+            //method calculates distances between two points according to their latitude and longitude
+            Location.distanceBetween(MainActivity.upLatitude, MainActivity.upLongitude, keptLat, keptLng, HDistanceResults);// DEFAULT  IN KILOMETERS
+
+
+
+            //FETCH SETTINGS RESULTS FROM SharedPreferences
+            //set Shared Preferences (there you save settings values )
+            SharedPreferences sharedFPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+            //get value from SharedPrefs
+            hPreference  = sharedFPreferences.getString("list_preference_units", "kilometre");//list_preference_units is key(id) of preference item in preferences.xml
+
+            //check settings results
+            if(hPreference.equals("kilometre") ) {
+                roundedHDis =  (double)Math.round( (HDistanceResults[0]/1000 ) * 100d) / 100d;//number of zeros must be same in and outside parenthesis.number of zeroes equals to number of numbers after dot that will remain after rounding up
+                hDistance.setText(roundedHDis + " km ");//km
+            }else{
+                roundedHDis =  (double)Math.round( (((HDistanceResults[0]*0.621371)/1000 ) ) * 100d) / 100d;//number of zeros must be same in and outside parenthesis.number of zeroes equals to number of numbers after dot that will remain after rounding up
+               hDistance.setText(roundedHDis + "  miles");//miles
+            }
+
 
 
 
--- app/src/main/java/com/happytrees/finalproject/database/LastSearch.java
+++ app/src/main/java/com/happytrees/finalproject/database/LastSearch.java
@@ -2,9 +2,12 @@ package com.happytrees.finalproject.database;
 
 import com.orm.SugarRecord;
 
-/**
- * Created by Boris on 3/9/2018.
- */
+
+/*NOTE: Sugar ORM has int field ID but Retrofit's Gson has also field ID of String type .In order to prevent crashes you need create additional java class
+which will extend SugarRecord,have variables with same names contained in original Json document and in time of creating objects will receive same values which its related
+module class's objects received using constructor in both times.
+This way we will store objects made by TxtResult class through ResultDB class in Sugar Orm database by converting json objects to java objects and only then store them*/
+//NOTE: there no need in recreating module hierarchy ,the only requirement is that variables of both classes (ResultDB and TxtResult/NearbyResult will have same names and values)
 
 public class LastSearch extends SugarRecord {
 
