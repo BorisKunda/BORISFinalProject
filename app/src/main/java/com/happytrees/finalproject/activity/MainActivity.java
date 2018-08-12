98136b3f7a5f61a3d9ee40faf334b77de203500e
limudimwork <boriskunda@gmail.com>
2018-08-12 23:57:49 +0300

    app locked to portrait mode only

--- app/src/main/java/com/happytrees/finalproject/activity/MainActivity.java
+++ app/src/main/java/com/happytrees/finalproject/activity/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -104,6 +105,10 @@ public class MainActivity extends AppCompatActivity  implements FragmentChanger{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //lock screen orientation on portrait
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
 
         //CHANGE ACTION BAR COLOR
